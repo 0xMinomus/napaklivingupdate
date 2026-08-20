0abfc41006f632b3e2643894bce90c24a7bb74b2
Andika Putra <zikopaak@gmail.com>
2026-08-20 22:15:12 +0800

    hapus backend, docker, dan file yang tidak terpakai

--- server/src/routes/categories.ts
+++
@@ -1,7 +0,0 @@
-import type { FastifyInstance } from 'fastify'
-import { categoryController } from '../controllers/category.controller.js'
-
-export async function categoryRoutes(app: FastifyInstance) {
-  app.get('/categories/:slug', categoryController.detail)
-  app.get('/categories', categoryController.list)
-}
--- server/src/routes/collections.ts
+++
@@ -1,7 +0,0 @@
-import type { FastifyInstance } from 'fastify'
-import { collectionController } from '../controllers/collection.controller.js'
-
-export async function collectionRoutes(app: FastifyInstance) {
-  app.get('/collections/:slug', collectionController.detail)
-  app.get('/collections', collectionController.list)
-}
