eb7b96ef8198ac0d899829927d76c8940599c863
Andika Putra <zikopaak@gmail.com>
2026-09-13 21:33:33 +0800

    hapus efek tilt 3D, sisakan animasi biasa

--- src/components/PageEffects.tsx
+++ src/components/PageEffects.tsx
@@ -3,21 +3,17 @@ import { Outlet } from 'react-router-dom'
 import { useScrollReveal } from '../hooks/useScrollReveal'
 import { useCardHover } from '../hooks/useCardHover'
 import { useImageReveal } from '../hooks/useImageReveal'
-import { useTilt } from '../hooks/useTilt'
 
 export default function PageEffects(): ReactElement {
   const revealRef = useScrollReveal()
   const hoverRef = useCardHover()
   const imageRef = useImageReveal()
-  const tiltRef = useTilt()
 
   return (
     <div ref={revealRef}>
       <div ref={hoverRef}>
         <div ref={imageRef}>
-          <div ref={tiltRef}>
-            <Outlet />
-          </div>
+          <Outlet />
         </div>
       </div>
     </div>
